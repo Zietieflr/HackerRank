ad685225755e242fe902ee1387115a028d4cd056
Zietieflr <lrmcguire93@gmail.com>
2020-10-01 16:56:34 -0600

    Add files for challenge. Initialize testing for javascript.

---
+++ javascript/forming-a-magic-square/forming-a-magic-square.js
@@ -0,0 +1,7 @@
+function formingMagicSquare(s) {
+  return s; 
+}
+
+module.exports = {
+  formingMagicSquare,
+}
---
+++ javascript/forming-a-magic-square/forming-a-magic-square.test.js
@@ -0,0 +1,7 @@
+const { formingMagicSquare } = require("./forming-a-magic-square")
+
+describe("forming-a-magic-square.js", () => {
+  test("first test", () => {
+    expect(3).toBe(4);
+  });
+});
