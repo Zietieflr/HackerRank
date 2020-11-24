482bda241c99194731f7cfb8c52741276bddcd89
Zietieflr <lrmcguire93@gmail.com>
2020-11-24 12:10:15 -0700

    HackerRank challenge optimized, with tests. Array Manipulation.

--- javascript/array-manipulation/array-manipulation.js
+++ javascript/array-manipulation/array-manipulation.js
@@ -1,18 +1,20 @@
 function arrayManipulation(length, queries) {
-  // Brute force solution
-  // O(n*(queries.length+1))
+  // O(2n + queries.length)
   let baseZeroList = [];
-  let max = 0;
-  for (let i=0; i<=length; i++) {
+  for (let i=0; i<length; i++) {
     baseZeroList[i] = 0;
-  }
+  };
   queries.forEach(query => {
-    let addend = query[2];
-    for (let i=query[0]; i<=query[1]; i++) {
-      baseZeroList[i] += addend;
-      if (baseZeroList[i] > max) {max = baseZeroList[i]};
-    }
-  })
+    const [start, end, modifier] = query;
+    baseZeroList[start-1] += modifier;
+    if (end<baseZeroList.length) {baseZeroList[end] -= modifier};
+  });
+  let max = baseZeroList[0];
+  baseZeroList.reduce((sum, baseZeroElement) => {
+    const tempSum = sum + baseZeroElement;
+    if (tempSum > max) {max = tempSum};
+    return tempSum;
+  }, 0);
   return max;
 }
 
