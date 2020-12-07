daef4caace70c6187e60602e2bed924cbf34397c
Zietieflr <lrmcguire93@gmail.com>
2020-12-07 16:00:41 -0700

    Execute plan. Fails when letters alternate. Rethink how substrings generated.

--- javascript/sherlockAndAnagrams/sherlockAndAnagrams.js
+++ javascript/sherlockAndAnagrams/sherlockAndAnagrams.js
@@ -6,11 +6,69 @@ function sherlockAndAnagrams(string) {
       // therefore have two objects
       // compare all object VALUES at each iteration
         // ^^ pivotal step for this idea to work
-      // if both objects are equal based off values anagrams++
+      // if both objects are equal based off values anagramsCount++
     // iterate for all letters that match (including if there are 3+ copies of a letter)
-  // return anagrams
+  // return anagramsCount
+  
+  let anagramsCount = 0;
+  const [ letterIndexes, repeatedLetters ] = lettersIndex(string);
+  if (repeatedLetters.length === 0) {return 0};
+  for (let letter of repeatedLetters) {
+    const indexList = letterIndexes[letter];
+    for (let i=0; i<indexList.length; i++) {
+      for (let i2=i+1; i2<indexList.length; i2++) {
+        const upLetters = {};
+        const downLetters = {};
+        const upIndex = indexList[i];
+        const downIndex = indexList[i2];
+        for (let i3=0; i3<downIndex-upIndex; i3++) {
+          const upLetter = string.charAt(upIndex+i3);
+          const downLetter = string.charAt(downIndex-i3);
+          if (!upLetters[upLetter]) {
+            upLetters[upLetter] = 1;
+          } else {
+            upLetters[upLetter]++;
+          }
+          if (!downLetters[downLetter]) {
+            downLetters[downLetter] = 1;
+          } else {
+            downLetters[downLetter]++;
+          }
+          if (isShallowObjectEqual(upLetters, downLetters)) {
+            anagramsCount++;
+          }
+        }
+      };
+    };
+  };
+  return anagramsCount;
+}
+
+function lettersIndex(string) {
+  const letterIndexes = {};
+  const repeatedLetters = [];
+  for (let i=0; i<string.length; i++) {
+    let letter = string.charAt(i);
+    if (!letterIndexes[letter]) {
+      letterIndexes[letter] = [i];
+    } else {
+      letterIndexes[letter].push(i);
+    };
+    if (letterIndexes[letter].length === 2) {repeatedLetters.push(letter)};
+  }
+  return [letterIndexes, repeatedLetters];
+}
+
+function isShallowObjectEqual(object1, object2) {
+  const firstKeys = Object.keys(object1); 
+  const secondKeys = Object.keys(object2);
+  if (firstKeys.length !== secondKeys.length) {return false};
+  for (let key of firstKeys) {
+    if(object1[key] !== object2[key]) {return false};
+  }
+  return true;
 }
 
 module.exports = {
-  sherlockAndAnagrams,
+  sherlockAndAnagrams, lettersIndex, isShallowObjectEqual
 }
--- javascript/sherlockAndAnagrams/sherlockAndAnagrams.test.js
+++ javascript/sherlockAndAnagrams/sherlockAndAnagrams.test.js
@@ -1,4 +1,4 @@
-const { sherlockAndAnagrams } = require("./sherlockAndAnagrams");
+const { sherlockAndAnagrams, lettersIndex, isShallowObjectEqual } = require("./sherlockAndAnagrams");
 
 describe("Given a string return the number of substrings that are anagrams "
   +"to each other.",
@@ -48,6 +48,47 @@ describe("Given a string return the number of substrings that are anagrams "
       const received = "qwertyewquioweqpaseqwdfgqwehjkqwe";
       const expected = 0;
       expect(sherlockAndAnagrams(received)).toBe(expected);
+    });
+  });
+  describe("isShallowObjectEqual", () => {
+    test("Given two objects with different counts; return false.", () => {
+      const received = [{z: 1, x: 1, q: 1, y: 1}, {z: 1, x: 1, y: 1}];
+      const expected = false;
+      expect(isShallowObjectEqual(...received)).toBe(expected);
+    });
+    test("Given two objects; return false.", () => {
+      const received = [{z: 1, x: 1, q: 1, y: 1}, {z: 1, x: 1, q: 1, y: 2}];
+      const expected = false;
+      expect(isShallowObjectEqual(...received)).toBe(expected);
+    });
+    test("Given two objects; return true.", () => {
+      const received = [{z: 1, x: 1, q: 1, y: 1}, {x: 1, z: 1, y: 1, q: 1}];
+      const expected = true;
+      expect(isShallowObjectEqual(...received)).toBe(expected);
+    });
+    test("Given two empty objects; return true.", () => {
+      const received = [{}, {}];
+      const expected = true;
+      expect(isShallowObjectEqual(...received)).toBe(expected);
+    });
+  });
+  describe("lettersIndex", () => {
+    test("Given a; return [{a: [0]}, []]", () => {
+      const received = "a";
+      const expected = [{a: [0]}, []];
+      expect(lettersIndex(received)).toStrictEqual(expected);
+    });
+    test("Given aa; return [{a: [0, 1]}, ['a']]", () => {
+      const received = "aa";
+      const expected = [{a: [0, 1]}, ["a"]];
+      expect(lettersIndex(received)).toStrictEqual(expected);
+    });
+    test("Given aaaabcc; "
+      +"return [{a: [0, 1, 2, 3], b: [4], c: [5, 6]}, ['a', 'c']]",
+    () => {
+      const received = "aaaabcc";
+      const expected = [{a: [0, 1, 2, 3], b: [4], c: [5, 6]}, ["a", "c"]];
+      expect(lettersIndex(received)).toStrictEqual(expected);
     });
   });
 });
