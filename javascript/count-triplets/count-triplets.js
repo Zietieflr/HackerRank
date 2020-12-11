f2fd8faa3793272a1776a3717ab2fff5f27c3012
Zietieflr <lrmcguire93@gmail.com>
2020-12-11 12:57:17 -0700

    HackerRank challenge passing local tests.

--- javascript/count-triplets/count-triplets.js
+++ javascript/count-triplets/count-triplets.js
@@ -11,9 +11,55 @@ function countTriplets(integers, ratio) {
           // Count all for which this is true (lower and higher)
           // Compare the tally of lower and higher
             // Add to count the least of the two numbers
-  return 0;
+  const integersLookUp = convertToListLookUp(integers);
+  let count = 0;
+  for (const key in integersLookUp) {
+    count += hasGeometricProgression(key, ratio, integersLookUp);
+  }
+  return count;
+}
+
+function convertToListLookUp(array) {
+  const lookUp = {};
+  array.forEach((element, index) => {
+    lookUp[element] ? lookUp[element].push(index) : lookUp[element] = [index];
+  });
+  return lookUp;
+}
+
+function hasGeometricProgression(lowerInt, ratio, lookUpTable) {
+  const lowerIntIndexes = lookUpTable[lowerInt];
+  const middleIntIndexes = lookUpTable[lowerInt*ratio];
+  const upperIntIndexes = lookUpTable[lowerInt*ratio**2];
+  let count = 0;
+  if (!middleIntIndexes || !upperIntIndexes) {
+    return count;
+  } else {
+    // for (let i=0; i<lowerIntIndexes.length; i++) {
+    //   const lowerIndex = lowerIntIndexes[i];
+    //   for (let i2=0; i2<middleIntIndexes.length; i2++) {
+    //     const middleIndex = middleIntIndexes[i2];
+    //     if (lowerIndex < middleIndex) {
+    //       for (let i3=0; i3<upperIntIndexes.length; i3++) {
+    //         const upperIndex = upperIntIndexes[i3];
+    //         count += middleIndex < upperIndex ? 1 : 0;
+    //       }
+    //     }
+    //   }
+    // }
+    lowerIntIndexes.forEach(lowerIndex => {
+      middleIntIndexes.forEach(middleIndex => {
+        if (lowerIndex < middleIndex) {
+          upperIntIndexes.forEach(upperIndex => {
+            count += middleIndex < upperIndex ? 1 : 0;
+          })
+        }
+      })
+    })
+    return count;
+  }
 }
 
 module.exports = {
-  countTriplets,
+  countTriplets, convertToListLookUp, hasGeometricProgression
 }
--- javascript/count-triplets/count-triplets.test.js
+++ javascript/count-triplets/count-triplets.test.js
@@ -1,4 +1,4 @@
-const { countTriplets } = require("./count-triplets");
+const { countTriplets, convertToListLookUp, hasGeometricProgression } = require("./count-triplets");
 
 describe("Given a list of integers, determine how many triplets have a "
   +"geometric progression.",
@@ -49,8 +49,8 @@ describe("Given a list of integers, determine how many triplets have a "
       const expected = 4;
       expect(countTriplets(...received)).toBe(expected);
     });
-    test("Given [1, 125, 5, 25, 5], 25; return 1.", () => {
-      const received = [[1, 5, 5, 25, 125], 5];
+    test("Given [1, 125, 5, 25, 5], 5; return 1.", () => {
+      const received = [[1, 125, 5, 25, 5], 5];
       const expected = 1;
       expect(countTriplets(...received)).toBe(expected);
     });
@@ -58,8 +58,54 @@ describe("Given a list of integers, determine how many triplets have a "
       +"return 7.", 
     () => {
       const received = [[1, 4, 17, 2, 4, 6, 3, 34, 8, 12, 8, 68, 16, 68], 2];
-      const expected = 7;
+      const expected = 9;
       expect(countTriplets(...received)).toBe(expected);
+    });
+  });
+  describe("convertToListLookUp", () => {
+    test("Given [1]; return {1: [0]}.", () => {
+      const received = [1];
+      const expected = {1: [0]};
+      expect(convertToListLookUp(received)).toStrictEqual(expected);
+    });
+    test("Given [1, 2, 3, 4, 4, 4, 5]; "
+      +"return {1: [0], 2: [1], 3: [2], 4: [3, 4, 5], 5: [5]}.", 
+    () => {
+      const received = [1, 2, 3, 4, 4, 4, 5];
+      const expected = {1: [0], 2: [1], 3: [2], 4: [3, 4, 5], 5: [6]};
+      expect(convertToListLookUp(received)).toStrictEqual(expected);
+    });
+  });
+  describe("hasGeometricProgression", () => {
+    test("Given 4, 2, {4: [0, 1]}; return 0.", () => {
+      const received = [4, 2, {4: [0, 1]}];
+      const expected = 0;
+      expect(hasGeometricProgression(...received)).toBe(expected);
+    });
+    test("Given 4, 2, {2: [0], 4: [1], 8: [2]}; return 1.", () => {
+      const received = [2, 2, {2: [0], 4: [1], 8: [2]}];
+      const expected = 1;
+      expect(hasGeometricProgression(...received)).toBe(expected);
+    });
+    test("Given 2, 2, {2: [1], 4: [0], 8: [2]}; return 0.", () => {
+      const received = [4, 2, {2: [1], 4: [0], 8: [2]}];
+      const expected = 0;
+      expect(hasGeometricProgression(...received)).toBe(expected);
+    });
+    test("Given 2, 2, {2: [0, 1], 4: [2], 8: [3]}; return 2.", () => {
+      const received = [2, 2, {2: [0, 1], 4: [2], 8: [3]}];
+      const expected = 2;
+      expect(hasGeometricProgression(...received)).toBe(expected);
+    });
+    test("Given 2, 2, {2: [0], 4: [1], 8: [2, 3]}; return 2.", () => {
+      const received = [2, 2, {2: [0], 4: [1], 8: [2, 3]}];
+      const expected = 2;
+      expect(hasGeometricProgression(...received)).toBe(expected);
+    });
+    test("Given 2, 2, {2: [0, 1], 4: [2, 3], 8: [4, 5]}; return 8.", () => {
+      const received = [2, 2, {2: [0, 1], 4: [2, 3], 8: [4, 5]}];
+      const expected = 8;
+      expect(hasGeometricProgression(...received)).toBe(expected);
     });
   });
 });
